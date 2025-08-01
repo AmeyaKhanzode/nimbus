7246cc0ec8fb2ca6dfb0d90db45281f534ae1365
Ameya Khanzode <ameyakhanzode2022@gmail.com>
2025-08-01 16:24:29 +0530

    testing with pi

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
-from pydantic import BaseModel
+from fastapi.responses import FileResponse
 import base64
 import os
 
 app = FastAPI()
-
 @app.post("/upload")
 async def upload_files(file: UploadFile = File(...)):
     content = await file.read()
@@ -12,9 +11,17 @@ async def upload_files(file: UploadFile = File(...)):
         os.makedirs("static", exist_ok=True)
         with open(f"static/{file.filename}", "wb") as f:
             f.write(content)
-    except Exception as e:
+    except FileNotFoundError as e:
         raise HTTPException(status_code=500, detail=f"File upload fail: {str(e)}")
     return {
             "filename": file.filename,
             "status": "File uploaded successfully"
         }
+
+@app.get("/download")
+async def download_files(filename: str):
+    with open(f"static/{filename}", "rb") as f:
+        content = f.read()
+    os.makedirs("cloudbox_downloads", exist_ok=True)
+    with open(f"cloudbox_downloads/{filename}", "wb") as f:
+        f.write(content)
--- test_api.py
+++ test_api.py
@@ -1,7 +1,9 @@
 import requests
+try:
+    with open("Ameya_Resme_SDE.pdf", "rb") as f:
+        files = {"file": ("Ameya_Resume_SDE.pdf", f, "application/pdf")}
+        response = requests.post("http://127.0.0.1:8000/upload", files=files)
 
-with open("Ameya_Resume_SDE.pdf", "rb") as f:
-    files = {"file": ("Ameya_Resume_SDE.pdf", f, "application/pdf")}
-    response = requests.post("http://127.0.0.1:8000/upload", files=files)
-
-print(response.json())
+    print(response.json())
+except FileNotFoundError:
+    print({"status": "File not found on client", "status_code": 404})
